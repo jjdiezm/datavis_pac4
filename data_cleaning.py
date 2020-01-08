2d11572aa2bfb7248019df5a84e6fea9eccd7b23
Juanjo Diez <jdiez@xmltravelgate.com>
2020-01-08 17:23:59 +0100

    update chord diagram

---
+++ data_cleaning.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import codecs
+
+# original_file downloaded from https://www.peaceagreements.org/
+# GWNO_file Got from http://www.correlatesofwar.org/ (Data Sets > State System Membership (v2016))
+original_file = "pax_all_agreements_data.xlsx"
+sandkey_file = "sandkey.txt"
+
+df_original = pd.read_excel(original_file)
+# print(df_original.describe())
+df_no_con = df_original[df_original["Loc1ISO"].isnull()]
+# Assure no one has no Loc1ISO
+print("Number of agreements without Loc1ISO: ".format(df_no_con.AgtId.count()))
+
+# Prepare data to SandKeying
+# Regions > Conflict Type > Status
+# Regions > Agreement Stage > Agreement/Conflict Type
+regions = df_original.Reg.unique()
+conflict_types = df_original.Contp.unique()
+status = df_original.Status.unique()
+stages = df_original.Stage.unique()
+agreement_types = df_original.Agtp.unique()
+
+print(regions)
+print(conflict_types)
+print(status)
+print(stages)
+print(agreement_types)
+with codecs.open(sandkey_file, 'w', encoding='utf8') as f:
+    f.write('1st SandKey: Regions > Conflict Type \n')
+    for continent in regions:
+        for conflict in conflict_types:
+            counted = df_original[(df_original.Reg == continent) & (df_original.Contp == conflict)].AgtId.count()
+            f.write(continent+'['+str(counted)+']'+conflict+'\n')
+    f.write('\n\n1st SandKey: Conflict Type > Status \n')
+    for conflict in conflict_types:
+        for state in status:
+            counted = df_original[(df_original.Contp == conflict) & (df_original.Status == state)].AgtId.count()
+            f.write(conflict+'['+str(counted)+']'+state+'\n')
+    f.write('\n\n2nd SandKey: Regions > Agreement Stage \n')
+    for continent in regions:
+        for stage in stages:
+            counted = df_original[(df_original.Reg == continent) & (df_original.Stage == stage)].AgtId.count()
+            f.write(continent+'['+str(counted)+']'+stage+'\n')
+    f.write('\n\n2nd SandKey: Agreement Stage > Agreement/Conflict Type \n')
+    for stage in stages:
+        for agreement_type in agreement_types:
+            counted = df_original[(df_original.Stage == stage) & (df_original.Agtp == agreement_type)].AgtId.count()
+            f.write(stage+'['+str(counted)+']'+agreement_type+'\n')
+print("Data prepared for sandkey at: "+sandkey_file)
