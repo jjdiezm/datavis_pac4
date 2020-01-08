3018881b31ff5695a64f2f7bd9f70e7237e4c410
Juanjo Diez <jdiez@xmltravelgate.com>
2020-01-08 20:50:27 +0100

    Updates on tableau and Chord diagram

--- data_cleaning.py
+++ data_cleaning.py
@@ -6,6 +6,7 @@ import codecs
 # GWNO_file Got from http://www.correlatesofwar.org/ (Data Sets > State System Membership (v2016))
 original_file = "pax_all_agreements_data.xlsx"
 sandkey_file = "sandkey.txt"
+chord_file = "chord.csv"
 
 df_original = pd.read_excel(original_file)
 # print(df_original.describe())
@@ -49,3 +50,14 @@ with codecs.open(sandkey_file, 'w', encoding='utf8') as f:
             counted = df_original[(df_original.Stage == stage) & (df_original.Agtp == agreement_type)].AgtId.count()
             f.write(stage+'['+str(counted)+']'+agreement_type+'\n')
 print("Data prepared for sandkey at: "+sandkey_file)
+df_loc2_con = df_original[df_original["Loc2ISO"].notnull()]
+loc1codes = df_loc2_con.Loc1ISO.unique()
+loc2codes = df_loc2_con.Loc2ISO.unique()
+with codecs.open(chord_file, 'w', encoding='utf8') as f:
+    f.write('from,to,count\n')
+    for loc1 in loc1codes:
+        for loc2 in loc2codes:
+            counted = df_loc2_con[(df_loc2_con.Loc1ISO == loc1) & (df_loc2_con.Loc2ISO == loc2)].AgtId.count()
+            if int(counted) > 0:
+                f.write(loc1+','+loc2+','+str(counted)+'\n')
+print("Data prepared for Chord at: "+chord_file)
